e1b91e342c461488fb2a9da5a5148b8497a60f68
Barbara Makarova <makarena2001barbara@yandex.ru>
2022-11-15 15:56:44 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 2000;
+        int oneBonus = 20;
+
+        int bonusMiles = ticketCost / oneBonus;
+        System.out.println(bonusMiles);
+    }
+}
